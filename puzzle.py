18c10a29cb2e0a232fe4b8f2dc2842591973b41d
planks123 <56295106+planks123@users.noreply.github.com>
2021-03-10 06:41:10 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,395 +1,127 @@
-import argparse
+# -*- coding: utf-8 -*-
+from heapq import heappush, heappop
+from random import shuffle
 import time
-import timeit
-from collections import deque
 
 
-#Information/////////////////////////////////////////////////////////
-class EightPuzzle:
-    def __init__(self, state, parent, move, depth, cost, key):
-        self.state = state
-        self.parent = parent
-        self.move = move
-        self.depth = depth
-        self.cost = cost
-        self.key = key
-        if self.state:
-            self.map = ''.join(str(e) for e in self.state)
-    def __eq__(self, other):
-        return self.map == other.map
-    def __lt__(self, other):
-        return self.map < other.map
-    def __str__(self):
-        return str(self.map)    
+class Solver:
+  def __init__(self, initial_state=None):
+    self.initial_state = State(initial_state)
+    self.goal = range(1, 9)
 
-#Global variables***********************************************
-GoalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-GoalNode = None # at finding solution
-NodesExpanded = 0 #total nodes visited
-MaxSearchDeep = 0 #max deep
-MaxFrontier = 0 #max frontier
+  def _rebuildPath(self, end):
+    path = [end]
+    state = end.parent
+    while state.parent:
+      path.append(state.parent)
+      state = state.parent
+    return path
+
+  def solve(self):
+    openset = PriorityQueue()
+    openset.add(self.initial_state)
+    closed = set()
+    moves = 0
+    print 'tentando resolver:'
+    print openset.peek(), '\n\n'
+    start = time.time()
+    while openset:
+      current = openset.poll()
+      if current.values[:-1] == self.goal:
+        end = time.time()
+        print 'achei uma solução'
+        path = self._rebuildPath(current)
+        for state in reversed(path):
+          print state
+          print
+        print 'resolvido com %d movimentos' % len(path)
+        print 'encontrei a solução em %2.f segundos' % float(end - start)
+        break
+      moves += 1
+      for state in current.possible_moves(moves):
+        if state not in closed:
+          openset.add(state)
+      closed.add(current)
+    else:
+      print 'não consegui solucionar!'
 
 
-#BFS**************************************************************
-def bfs(startState):
+class State:
+  def __init__(self, values, moves=0, parent=None):
+    self.values = values
+    self.moves = moves
+    self.parent = parent
+    self.goal = range(1, 9)
 
-    global MaxFrontier, GoalNode, MaxSearchDeep
+  def possible_moves(self, moves):
+    i = self.values.index(0)
+    if i in [3, 4, 5, 6, 7, 8]:
+      new_board = self.values[:]
+      new_board[i], new_board[i - 3] = new_board[i - 3], new_board[i]
+      yield State(new_board, moves, self)
+    if i in [1, 2, 4, 5, 7, 8]:
+      new_board = self.values[:]
+      new_board[i], new_board[i - 1] = new_board[i - 1], new_board[i]
+      yield State(new_board, moves, self)
+    if i in [0, 1, 3, 4, 6, 7]:
+      new_board = self.values[:]
+      new_board[i], new_board[i + 1] = new_board[i + 1], new_board[i]
+      yield State(new_board, moves, self)
+    if i in [0, 1, 2, 3, 4, 5]:
+      new_board = self.values[:]
+      new_board[i], new_board[i + 3] = new_board[i + 3], new_board[i]
+      yield State(new_board, moves, self)
 
-    boardVisited= set()
-    Queue = deque([EightPuzzle(startState, None, None, 0, 0, 0)])
+  def score(self):
+    return self._h() + self._g()
 
-    while Queue:
-        node = Queue.popleft()
-        boardVisited.add(node.map)
-        if node.state == GoalState:
-            GoalNode = node
-            return Queue
-        posiblePaths = subNodes(node)
-        for path in posiblePaths:
-            if path.map not in boardVisited:
-                Queue.append(path)
-                boardVisited.add(path.map)
-                if path.depth > MaxSearchDeep:
-                    MaxSearchDeep = MaxSearchDeep + 1
-        if len(Queue) > MaxFrontier:
-            QueueSize = len(Queue)
-            MaxFrontier = QueueSize
-            
-#DFS**************************************************************
-def dfs(startState):
+  def _h(self):
+    return sum([1 if self.values[i] != self.goal[i] else 0 for i in xrange(8)])
 
-    global MaxFrontier, GoalNode, MaxSearchDeep
+  def _g(self):
+    return self.moves
 
-    boardVisited = set()
-    stack = list([EightPuzzle(startState, None, None, 0, 0, 0)])
-    while stack:
-        node = stack.pop()
-        boardVisited.add(node.map)
-        if node.state == GoalState:
-            GoalNode = node
-            return stack
-        #inverse the order of next paths for execution porpuses
-        posiblePaths = reversed(subNodes(node))
-        for path in posiblePaths:
-            if path.map not in boardVisited:
-                stack.append(path)
-                boardVisited.add(path.map)
-                if path.depth > MaxSearchDeep:
-                    MaxSearchDeep = 1 + MaxSearchDeep
-        if len(stack) > MaxFrontier:
-            MaxFrontier = len(stack)
-    
+  def __cmp__(self, other):
+    return self.values == other.values
 
-#AST**************************************************************
-def ast(startState):
-    
-    global MaxFrontier, MaxSearchDeep, GoalNode
-    
-    #transform initial state to calculate Heuritic
-    node1 = ""
-    for poss in startState:
-        node1 = node1 + str(poss)
+  def __eq__(self, other):
+    return self.__cmp__(other)
 
-    #calculate Heuristic and set initial node
-    key = Heuristic(node1)
-    boardVisited= set()
-    Queue = []
-    Queue.append(EightPuzzle(startState, None, None, 0, 0, key)) 
-    boardVisited.add(node1)
-    
-    while Queue:
-        Queue.sort(key=lambda o: o.key) 
-        node = Queue.pop(0)
-        if node.state == GoalState:
-            GoalNode = node
-            return Queue
-        posiblePaths = subNodes(node)
-        for path in posiblePaths:      
-            thisPath = path.map[:]
-            if thisPath not in boardVisited:
-                key = Heuristic(path.map)
-                path.key = key + path.depth
-                Queue.append(path)               
-                boardVisited.add(path.map[:])
-                if path.depth > MaxSearchDeep:
-                    MaxSearchDeep = 1 + MaxSearchDeep
-        
-        
-#Heuristic: distance to root numbers
-values_0 = [0,1,2,1,2,3,2,3,4]
-values_1 = [1,0,1,2,1,2,3,2,3]
-values_2 = [2,1,0,3,2,1,4,3,2]
-values_3 = [1,2,3,0,1,2,1,2,3]
-values_4 = [2,1,2,1,0,1,2,1,2]
-values_5 = [3,2,1,2,1,0,3,2,1]
-values_6 = [2,3,4,1,2,3,0,1,2]
-values_7 = [3,2,3,2,1,2,1,0,1]
-values_8 = [4,3,2,3,2,1,2,1,0]
+  def __hash__(self):
+    return hash(str(self.values))
 
-def Heuristic(node):
+  def __lt__(self, other):
+    return self.score() < other.score()
 
-    global values_0,values_1,values_2,values_3,values_4,values_5,values_6,values_7,values_8   
-    v0=values_0[node.index("0")]
-    v1=values_1[node.index("1")]
-    v2=values_2[node.index("2")]
-    v3=values_3[node.index("3")]
-    v4=values_4[node.index("4")]
-    v5=values_5[node.index("5")]
-    v6=values_6[node.index("6")]
-    v7=values_7[node.index("7")]
-    v8=values_8[node.index("8")]
-    valorTotal = v0+v1+v2+v3+v4+v5+v6+v7+v8
-    return valorTotal
-    
-        
+  def __str__(self):
+    return '\n'.join([str(self.values[:3]),
+        str(self.values[3:6]),
+        str(self.values[6:9])]).replace('[', '').replace(']', '').replace(',', '').replace('0', 'x')
 
-    
-#Obtain Sub Nodes********************************************************
-def subNodes(node):
+class PriorityQueue:
+  def __init__(self):
+    self.pq = []
 
-    global NodesExpanded
-    NodesExpanded = NodesExpanded+1
+  def add(self, item):
+    heappush(self.pq, item)
 
-    nextPaths = []
-    nextPaths.append(EightPuzzle(move(node.state, 1), node, 1, node.depth + 1, node.cost + 1, 0))
-    nextPaths.append(EightPuzzle(move(node.state, 2), node, 2, node.depth + 1, node.cost + 1, 0))
-    nextPaths.append(EightPuzzle(move(node.state, 3), node, 3, node.depth + 1, node.cost + 1, 0))
-    nextPaths.append(EightPuzzle(move(node.state, 4), node, 4, node.depth + 1, node.cost + 1, 0))
-    nodes=[]
-    for procPaths in nextPaths:
-        if(procPaths.state!=None):
-            nodes.append(procPaths)
-    return nodes
+  def poll(self):
+    return heappop(self.pq)
 
-#Next step**************************************************************
-def move(state, direction):
-    #generate a copy
-    newState = state[:]
-    
-    #obtain poss of 0
-    index = newState.index(0)
+  def peek(self):
+    return self.pq[0]
 
-    if(index==0):
-        if(direction==1):
-            return None
-        if(direction==2):
-            temp=newState[0]
-            newState[0]=newState[3]
-            newState[3]=temp
-        if(direction==3):
-            return None
-        if(direction==4):
-            temp=newState[0]
-            newState[0]=newState[1]
-            newState[1]=temp
-        return newState      
-    if(index==1):
-        if(direction==1):
-            return None
-        if(direction==2):
-            temp=newState[1]
-            newState[1]=newState[4]
-            newState[4]=temp
-        if(direction==3):
-            temp=newState[1]
-            newState[1]=newState[0]
-            newState[0]=temp
-        if(direction==4):
-            temp=newState[1]
-            newState[1]=newState[2]
-            newState[2]=temp
-        return newState    
-    if(index==2):
-        if(direction==1):
-            return None
-        if(direction==2):
-            temp=newState[2]
-            newState[2]=newState[5]
-            newState[5]=temp
-        if(direction==3):
-            temp=newState[2]
-            newState[2]=newState[1]
-            newState[1]=temp
-        if(direction==4):
-            return None
-        return newState
-    if(index==3):
-        if(direction==1):
-            temp=newState[3]
-            newState[3]=newState[0]
-            newState[0]=temp
-        if(direction==2):
-            temp=newState[3]
-            newState[3]=newState[6]
-            newState[6]=temp
-        if(direction==3):
-            return None
-        if(direction==4):
-            temp=newState[3]
-            newState[3]=newState[4]
-            newState[4]=temp
-        return newState
-    if(index==4):
-        if(direction==1):
-            temp=newState[4]
-            newState[4]=newState[1]
-            newState[1]=temp
-        if(direction==2):
-            temp=newState[4]
-            newState[4]=newState[7]
-            newState[7]=temp
-        if(direction==3):
-            temp=newState[4]
-            newState[4]=newState[3]
-            newState[3]=temp
-        if(direction==4):
-            temp=newState[4]
-            newState[4]=newState[5]
-            newState[5]=temp
-        return newState
-    if(index==5):
-        if(direction==1):
-            temp=newState[5]
-            newState[5]=newState[2]
-            newState[2]=temp
-        if(direction==2):
-            temp=newState[5]
-            newState[5]=newState[8]
-            newState[8]=temp
-        if(direction==3):
-            temp=newState[5]
-            newState[5]=newState[4]
-            newState[4]=temp
-        if(direction==4):
-            return None
-        return newState
-    if(index==6):
-        if(direction==1):
-            temp=newState[6]
-            newState[6]=newState[3]
-            newState[3]=temp
-        if(direction==2):
-            return None
-        if(direction==3):
-            return None
-        if(direction==4):
-            temp=newState[6]
-            newState[6]=newState[7]
-            newState[7]=temp
-        return newState
-    if(index==7):
-        if(direction==1):
-            temp=newState[7]
-            newState[7]=newState[4]
-            newState[4]=temp
-        if(direction==2):
-            return None
-        if(direction==3):
-            temp=newState[7]
-            newState[7]=newState[6]
-            newState[6]=temp
-        if(direction==4):
-            temp=newState[7]
-            newState[7]=newState[8]
-            newState[8]=temp
-        return newState
-    if(index==8):
-        if(direction==1):
-            temp=newState[8]
-            newState[8]=newState[5]
-            newState[5]=temp
-        if(direction==2):
-            return None
-        if(direction==3):
-            temp=newState[8]
-            newState[8]=newState[7]
-            newState[7]=temp
-        if(direction==4):
-            return None
-        return newState
-    
-#MAIN**************************************************************
-def main():
+  def remove(self, item):
+    value = self.pq.remove(item)
+    heapify(self.pq)
+    return value is not None
 
-    global GoalNode
+  def __len__(self):
+    return len(self.pq)
 
-    #a = [1,8,2,3,4,5,6,7,0]
-    #point=Heuristic(a)
-    #print(point)
-    #return
-    
-    #info = "6,1,8,4,0,2,7,3,5" #20
-    #info = "8,6,4,2,1,3,5,7,0" #26
-    
-    #Obtain information from calling parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('method')
-    parser.add_argument('initialBoard')
-    args = parser.parse_args()
-    data = args.initialBoard.split(",")
 
-    #Build initial board state
-    InitialState = []
-    InitialState.append(int(data[0]))
-    InitialState.append(int(data[1]))
-    InitialState.append(int(data[2]))
-    InitialState.append(int(data[3]))
-    InitialState.append(int(data[4]))
-    InitialState.append(int(data[5]))
-    InitialState.append(int(data[6]))
-    InitialState.append(int(data[7]))
-    InitialState.append(int(data[8]))
-
-    #Start operation
-    start = timeit.default_timer()
-
-    function = args.method
-    if(function=="bfs"):
-        bfs(InitialState)
-    if(function=="dfs"):
-        dfs(InitialState)  
-    if(function=="ast"):
-        ast(InitialState) 
-
-    stop = timeit.default_timer()
-    time = stop-start
-
-    #Save total path result
-    deep=GoalNode.depth
-    moves = []
-    while InitialState != GoalNode.state:
-        if GoalNode.move == 1:
-            path = 'Up'
-        if GoalNode.move == 2:
-            path = 'Down'
-        if GoalNode.move == 3:
-            path = 'Left'
-        if GoalNode.move == 4:
-            path = 'Right'
-        moves.insert(0, path)
-        GoalNode = GoalNode.parent
-
-    #'''
-    #Print results
-    print("path: ",moves)
-    print("cost: ",len(moves))
-    print("nodes expanded: ",str(NodesExpanded))
-    print("search_depth: ",str(deep))
-    print("MaxSearchDeep: ",str(MaxSearchDeep))
-    print("running_time: ",format(time, '.8f'))
-    #'''
-
-    #Generate output document for grade system
-    #'''
-    file = open('output.txt', 'w')
-    file.write("path_to_goal: " + str(moves) + "\n")
-    file.write("cost_of_path: " + str(len(moves)) + "\n")
-    file.write("nodes_expanded: " + str(NodesExpanded) + "\n")
-    file.write("search_depth: " + str(deep) + "\n")
-    file.write("max_search_depth: " + str(MaxSearchDeep) + "\n")
-    file.write("running_time: " + format(time, '.8f') + "\n")
-    file.close()
-    #'''
-
-if __name__ == '__main__':
-    main()
+puzzle = range(9)
+shuffle(puzzle)
+#puzzle = [1, 7, 4, 6, 8, 3, 2, 5, 0]
+solver = Solver(puzzle)
+solver.solve()
